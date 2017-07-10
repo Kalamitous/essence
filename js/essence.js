e19c2cce683bce1969475cc1905b7f188ec92dd9
Kalamitous <xkalamitous@gmail.com>
2017-07-09 22:10:47 -0700

    Utilize web worker

--- js/essence.js
+++ js/essence.js
@@ -1,8 +1,3 @@
-// seed noise
-var date = new Date()
-var elevation = new SimplexNoise('rnd' + date.getTime())
-var moisture = new SimplexNoise('rnd' + date.getTime() + 1)
-
 // audio
 var audioCtx = new (window.AudioContext || window.webKitAudioContext)()
 var analyser = audioCtx.createAnalyser()
@@ -41,19 +36,6 @@ function decodeBuffer(data, callback) {
 	})
 }
 
-// constants
-var SIZE_X = 64
-var SIZE_Y = 4
-var QUALITY = 1
-var TILE_SIZE = 1 / QUALITY
-var SECTION_COUNT = (SIZE_X * QUALITY + 1) * (SIZE_Y * QUALITY + 1)
-var TOTAL_COUNT = (SIZE_X * QUALITY + 1) * (SIZE_X * QUALITY + 1)
-var CLOUD_NUM = 32
-var OCEAN_SIZE = SIZE_X * 4
-var VARIANCE = 4
-var CAM_HEIGHT = 16
-var SPEED = 0.05
-
 $(document).ready(function() {
 	// setup scene
     var scene = new THREE.Scene()
@@ -89,7 +71,7 @@ $(document).ready(function() {
 	var hemisphereLight = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.5)
 	var directionalLight = new THREE.DirectionalLight(0xffff55, 0.5)
 	directionalLight.position.x = camera.position.x
-	directionalLight.position.y = SIZE_X * QUALITY / 2
+	directionalLight.position.y = SIZE_X / 2
 	directionalLight.position.z = VARIANCE / 3
 	
 	scene.add(ambientLight)
@@ -266,12 +248,30 @@ $(document).ready(function() {
 	
 	var terrainGeometry = new THREE.PlaneGeometry(SIZE_X, SIZE_X, SIZE_X * QUALITY, SIZE_X * QUALITY)
 	terrain = buildTerrain(terrainGeometry)
+	
+	// initialize web worker
+	var count = 0
+	var worker = new Worker('js/worker.js')
+	worker.addEventListener('message', function(e) {
+		count += 1
+
+		var vertices = e.data
+		for(var i = 0; i < terrain.geometry.vertices.length; i++) {
+			terrain.geometry.vertices[i].z = e.data[i]
+		}
+		
+		terrain.geometry.verticesNeedUpdate = true
+		
+		if (count >= SIZE_Y) {
+			terrain.geometry.computeVertexNormals()
+			terrain.geometry.computeFaceNormals()
+		}
+		
+		terrain.position.y = 0
+	}, false)
 
 	// render loop
 	var frame = 0
-	var count = 0
-	var rowPos = []
-	var verts = []
     function render() {	
 		frame += 1
 		
@@ -291,133 +291,10 @@ $(document).ready(function() {
 		waterNormals.offset.y -= SPEED
 		water.render()
 
-		// get instantaneous audio volume
-		analyser.getByteFrequencyData(dataArray)
-      
-		var energySum = 0
-		for(var i = 0; i < dataArray.length; i++) {
-			energySum += dataArray[i]
-		}
-		
-		var avgEnergy = energySum / dataArray.length
-
-		// generate terrain
-		var mesh
-		var nextRowPos = []
-		var curOffset = []
 		if (frame % Math.floor(((1 / SPEED) * (SIZE_Y))) == 0) {
-			count += 1
-			
-			var curVerts = []
-			var newHeight = avgEnergy / 20
-			if (newHeight > 8 - VARIANCE / 3) {
-				newHeight = 8 - VARIANCE / 3
-			}
-				
-			for (var i = 0; i < SECTION_COUNT; i++) {
-				var row = Math.floor(i / (SIZE_X * QUALITY + 1)) + 1
-				var column = i % (SIZE_X * QUALITY + 1) + 1
-				
-				// generate elevation & moisture noise from audio volume
-				var x = TILE_SIZE * (column - 1) - SIZE_X / 2
-				var y = SIZE_Y * 2 - TILE_SIZE * ((SIZE_Y * QUALITY) * count) + TILE_SIZE * (row - 1)
+			analyser.getByteFrequencyData(dataArray)
 
-				var nx = x / SIZE_X
-				var ny = y / SIZE_X
-				
-				var e = (1 * genNoise(elevation, 1 * nx,  1 * ny)
-					   + (1 / 2) * genNoise(elevation, 2 * nx,  2 * ny)
-					   + (1 / 4) * genNoise(elevation, 4 * nx,  2 * ny)
-					   + (1 / 8) * genNoise(elevation, 8 * nx,  8 * ny)
-					   + (1 / 16) * genNoise(elevation, 16 * nx, 16 * ny)
-					   + (1 / 32) * genNoise(elevation, 32 * nx, 32 * ny))
-				e /= (1 + 1 / 2 + 1 / 4 + 1 / 8 + 1 / 16 + 1 / 32)
-				e = e ** 4
-				var m = (1 * genNoise(moisture, 1 * nx,  1 * ny)
-					   + (1 / 2) * genNoise(moisture, 2 * nx,  2 * ny)
-					   + (1 / 4) * genNoise(moisture, 4 * nx,  2 * ny)
-					   + (1 / 8) * genNoise(moisture, 8 * nx,  8 * ny)
-					   + (1 / 16) * genNoise(moisture, 16 * nx, 16 * ny)
-					   + (1 / 32) * genNoise(moisture, 32 * nx, 32 * ny))
-				m /= (1 + 1 / 2 + 1 / 4 + 1 / 8 + 1 / 16 + 1 / 32)
-				var value = e * ((newHeight + m) * VARIANCE) + (newHeight / 16 - (VARIANCE / 3 - 1))
-				
-				// edge falloffs
-				if (column <= 16 * QUALITY) {
-					value *= (column - 1) / (16 * QUALITY - 1)
-				} else if (column >= SIZE_X * QUALITY - 16 * QUALITY) {
-					value *= (Math.abs(column - SIZE_X * QUALITY) - 1) / (16 * QUALITY - 1)
-				}
-				if (count == 1) {
-					value *= (Math.floor(SECTION_COUNT / (SIZE_X * QUALITY + 1)) - row) / (Math.floor(SECTION_COUNT / (SIZE_X * QUALITY + 1)) - 1)
-				}
-				
-				// give curve
-				var offsetColumn = i % (SIZE_X * 1 + 1) + 1
-				var offset = ((1 - easeInQuad(offsetColumn / (SIZE_X + 1)))) * 16
-				if (offsetColumn < (SIZE_X) / 2 + 1) {
-					offset = ((1 - easeInQuad(((SIZE_X + 1) - offsetColumn) / (SIZE_X + 1)))) * 16
-				}
-				offset /= ((OCEAN_SIZE / SIZE_X) ** 2)
-				
-				value += offset
-				
-				terrain.geometry.vertices[i].z = value
-
-				if (row == 1) {
-					nextRowPos.push(value)
-				} else if (row == (SIZE_Y * QUALITY) + 1) {
-					if (rowPos.length > 0) {
-						curOffset.push(value - rowPos[column])
-					} else {
-						curOffset.push(0)
-					}
-				}
-			}
-
-			// interpolate terrain altitudes to avoid distinct bumps
-			for (var i = 0; i < SECTION_COUNT; i++) {
-				var row = Math.floor(i / (SIZE_X * QUALITY + 1)) + 1
-				var column = i % (SIZE_X * QUALITY + 1) + 1
-				
-				var increment = curOffset[column]
-				var step = ((SIZE_Y * QUALITY) - (row - 1)) / (SIZE_Y * QUALITY)
-				var lerp = (increment * easeInOutQuad(step))
-
-				if (increment && lerp) {
-					terrain.geometry.dynamic = true
-					terrain.geometry.vertices[i].z += lerp - increment
-					terrain.geometry.verticesNeedUpdate = true
-				}
-				
-				curVerts.push(terrain.geometry.vertices[i].z)
-			}
-			
-			// scroll terrain
-			if (verts.length > 0) {
-				for (var i = 0; i < verts.length; i++) {
-					for (var j = 0; j < SECTION_COUNT; j++) {
-						var idx = SECTION_COUNT * (i + 1) + j - (SIZE_X * QUALITY + 1) * (i + 1)
-
-						terrain.geometry.dynamic = true
-						terrain.geometry.vertices[idx].z = verts[i][j]
-						terrain.geometry.verticesNeedUpdate = true
-					}
-				}
-			}
-			
-			terrain.geometry.computeVertexNormals()
-			terrain.geometry.computeFaceNormals()
-			
-			verts.unshift(curVerts)
-			
-			if (verts.length >= (SIZE_X / SIZE_Y)) {
-				verts.pop()
-			}
-			
-			terrain.position.y = 0
-			
-			rowPos = nextRowPos
+			worker.postMessage(dataArray)
 		}
 		
         renderer.render(scene, camera)
@@ -428,20 +305,3 @@ $(document).ready(function() {
         render()
     }, 1000 / 144);
 })
-
-// helper functions
-function genNoise(type, nx, ny) {
-	return (type.noise(nx, ny) / 2) + 0.5
-}
-
-function easeInQuad(t) {
-	return t ** t
-}
-
-function easeOutQuad(t) {
-	return t * (2 - t)
-}
-
-function easeInOutQuad(t) {
-	return t < 0.5 ? 2 * t ** 2:-1 + (4 - 2 * t) * t
-}
---
+++ js/globals.js
@@ -0,0 +1,29 @@
+// constants
+var SIZE_X = 64
+var SIZE_Y = 4
+var QUALITY = 1
+var TILE_SIZE = 1 / QUALITY
+var SECTION_COUNT = (SIZE_X * QUALITY + 1) * (SIZE_Y * QUALITY + 1)
+var TOTAL_COUNT = (SIZE_X * QUALITY + 1) * (SIZE_X * QUALITY + 1)
+var CLOUD_NUM = 32
+var OCEAN_SIZE = SIZE_X * 4
+var VARIANCE = 4
+var CAM_HEIGHT = 16
+var SPEED = 0.05
+
+// helper functions
+function genNoise(type, nx, ny) {
+	return (type.noise(nx, ny) / 2) + 0.5
+}
+
+function easeInQuad(t) {
+	return t ** t
+}
+
+function easeOutQuad(t) {
+	return t * (2 - t)
+}
+
+function easeInOutQuad(t) {
+	return t < 0.5 ? 2 * t ** 2:-1 + (4 - 2 * t) * t
+}
